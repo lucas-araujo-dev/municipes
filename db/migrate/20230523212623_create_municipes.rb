23fed22a6c512884171679d3358e8674ed76fe70
lucas araujo <lucas.trabalho.araujo@gmail.com>
2023-05-24 13:16:41 -0300

    ajustada migration de municipes

--- db/migrate/20230523212623_create_municipes.rb
+++ db/migrate/20230523212623_create_municipes.rb
@@ -1,14 +1,14 @@
 class CreateMunicipes < ActiveRecord::Migration[7.0]
   def change
     create_table :municipes do |t|
-      t.string :full_name
-      t.string :cpf, index: { unique: true }
-      t.string :cns, index: { unique: true }
-      t.string :email, index: { unique: true }
-      t.date :birthdate
-      t.string :phone
+      t.string :full_name, null: false
+      t.string :cpf, index: { unique: true }, null: false
+      t.string :cns, index: { unique: true }, null: false
+      t.string :email, index: { unique: true }, null: false
+      t.date :birthdate, null: false
+      t.string :phone, null: false
       t.string :picture
-      t.string :status
+      t.string :status, null: false, default: 'active'
 
       t.timestamps
     end
