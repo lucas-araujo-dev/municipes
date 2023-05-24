4820c84b093b5766893ee8539def161b2d9f88c5
lucas araujo <lucas.trabalho.araujo@gmail.com>
2023-05-24 13:17:33 -0300

    adicionadas configs em application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,13 +25,11 @@ module Municipes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
+
+    # Timezone and locale configuration
+    config.time_zone = 'Brasilia'
+    config.i18n.default_locale = 'pt-BR'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
