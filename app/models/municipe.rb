634682414b610718fa048ab5ac4533689dd5b1c8
lucas araujo <lucas.trabalho.araujo@gmail.com>
2023-05-26 16:45:26 -0300

    ajustado para nao chamar sms durante os testes

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -25,13 +25,13 @@ class Municipe < ApplicationRecord
     sms_message = I18n.t('sms.messages.welcome')
 
     MunicipeMailer.welcome(self).deliver_later
-    SmsService.new(phone, sms_message).send_sms
+    SmsService.new(phone, sms_message).send_sms unless Rails.env.test?
   end
 
   def notify_updated
     sms_message = I18n.t('sms.messages.updated')
 
     MunicipeMailer.updated(self).deliver_later
-    SmsService.new(phone, sms_message).send_sms
+    SmsService.new(phone, sms_message).send_sms unless Rails.env.test?
   end
 end
