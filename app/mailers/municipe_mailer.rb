be5c6243e1e102fc62cc451b8b28540e77dd5be6
lucas araujo <lucas.trabalho.araujo@gmail.com>
2023-05-26 15:39:49 -0300

    rubocop gem

--- app/mailers/municipe_mailer.rb
+++ app/mailers/municipe_mailer.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class MunicipeMailer < ApplicationMailer
   default from: 'municipes@systems.com'
 
   def welcome(municipe)
     @municipe = municipe
 
-    mail(to: @municipe.email, subject: 'Bem-vindo ao nosso aplicativo')
+    mail(to: @municipe.email, subject: t('mailers.municipe_mailer.created'))
   end
 
   def updated(municipe)
     @municipe = municipe
 
-    mail(to: @municipe.email, subject: 'Municipe Atualizado')
+    mail(to: @municipe.email, subject: t('mailers.municipe_mailer.updated'))
   end
 end
--- spec/mailers/municipe_mailer_spec.rb
+++ spec/mailers/municipe_mailer_spec.rb
@@ -1,36 +1,40 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe MunicipeMailer, type: :mailer do
+  include ActionView::Helpers::TranslationHelper
+
   describe 'welcome_email' do
     let(:municipe) { create(:municipe) }
-    let(:mail) { MunicipeMailer.welcome(municipe) }
+    let(:mail) { described_class.welcome(municipe) }
 
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Bem-vindo ao nosso aplicativo')
+    it 'renders the headers', :aggregate_failures do
+      expect(mail.subject).to eq(t('mailers.municipe_mailer.created'))
       expect(mail.to).to eq([municipe.email])
       expect(mail.from).to eq(['municipes@systems.com'])
     end
 
-    it 'renders the body' do
+    it 'renders the body', :aggregate_failures do
       expect(mail.body.encoded).to include(municipe.full_name)
-      expect(mail.body.encoded).to include("Cadastro Realizado com Sucesso")
+      expect(mail.body.encoded).to include(t('mailers.municipe_mailer.welcome_message'))
       expect(mail.body.encoded).to include("/municipes/#{municipe.id}/edit")
     end
   end
 
   describe 'updated_email' do
     let(:municipe) { create(:municipe) }
-    let(:mail) { MunicipeMailer.updated(municipe) }
+    let(:mail) { described_class.updated(municipe) }
 
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Municipe Atualizado')
+    it 'renders the headers', :aggregate_failures do
+      expect(mail.subject).to eq(t('mailers.municipe_mailer.updated'))
       expect(mail.to).to eq([municipe.email])
       expect(mail.from).to eq(['municipes@systems.com'])
     end
 
-    it 'renders the body' do
+    it 'renders the body', :aggregate_failures do
       expect(mail.body.encoded).to include(municipe.full_name)
-      expect(mail.body.encoded).to include("Cadastro Atualizado com Sucesso")
+      expect(mail.body.encoded).to include(t('mailers.municipe_mailer.updated'))
       expect(mail.body.encoded).to include("/municipes/#{municipe.id}/edit")
     end
   end
